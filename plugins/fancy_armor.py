c2eb652e2e52ad60ad241eddee0c6bf2bb046b0a
PuckiSilver <84644295+PuckiSilver@users.noreply.github.com>
2024-01-30 11:32:40 +0100

    add cit to fancy armor plugin

--- plugins/fancy_armor.py
+++ plugins/fancy_armor.py
@@ -1,5 +1,5 @@
 from typing import ClassVar, Tuple
-from beet import Context, PngFile, Texture
+from beet import Context, PngFile, Texture, TextFile
 from beet.contrib.vanilla import Vanilla
 from PIL import Image
 import math
@@ -7,14 +7,27 @@ import math
 
 class ArmorTexture(PngFile):
     """Class representing an armor texture to be converted"""
-
     scope: ClassVar[Tuple[str, ...]] = ("armor",)
+    extension: ClassVar[str] = ".png"
+
+
+class CitPropertiesFile(TextFile):
+    """Class representing the cit properties file"""
+    scope: ClassVar[Tuple[str, ...]] = ("optifine", "cit", "fancypantslite")
+    extension: ClassVar[str] = ".properties"
+
+
+class CitArmorTexture(PngFile):
+    """Class representing the cit properties file"""
+    scope: ClassVar[Tuple[str, ...]] = ("optifine", "cit", "fancypantslite")
     extension: ClassVar[str] = ".png"
 
 
 def setup(ctx: Context):
     ctx.assets.extend_namespace += [
         ArmorTexture,
+        CitPropertiesFile,
+        CitArmorTexture,
     ]
 
 
@@ -24,7 +37,9 @@ def get_leather_armor_textures(ctx: Context) -> Tuple[Image.Image, Image.Image]:
     client_jar = vanilla.mount('assets/minecraft/textures/models')
     return (
         client_jar.assets.textures['minecraft:models/armor/leather_layer_1'].copy().ensure_deserialized(),
-        client_jar.assets.textures['minecraft:models/armor/leather_layer_2'].copy().ensure_deserialized())
+        client_jar.assets.textures['minecraft:models/armor/leather_layer_2'].copy().ensure_deserialized(),
+        client_jar.assets.textures['minecraft:models/armor/leather_layer_1_overlay'].copy().ensure_deserialized(),
+        client_jar.assets.textures['minecraft:models/armor/leather_layer_2_overlay'].copy().ensure_deserialized(),)
 
 
 def color_int_to_tuple(color: int) -> Tuple[int, int, int]:
@@ -49,8 +64,25 @@ def stitch_texture(images: list[Image.Image]) -> Image.Image:
 def fancify(ctx: Context):
     if not ArmorTexture in ctx.assets: return
 
+    ctx.assets[CitPropertiesFile]['minecraft:base_leather_armor.properties'] = CitPropertiesFile(
+        '\n'.join([
+        "# Generated using https://github.com/PuckiSilver/FancyPantsLite-to-CIT by PuckiSilver",
+        "type=armor",
+        "items=minecraft:leather_helmet minecraft:leather_chestplate minecraft:leather_leggings minecraft:leather_boots",
+        "texture.leather_layer_1=base_leather_armor_1",
+        "texture.leather_layer_2=base_leather_armor_2",
+        "texture.leather_layer_1_overlay=base_leather_armor_1_overlay",
+        "texture.leather_layer_2_overlay=base_leather_armor_2_overlay",
+        "weight=-1",""]))
+
     trash = []
-    leather_armor_1, leather_armor_2 = get_leather_armor_textures(ctx)
+    leather_armor_1, leather_armor_2, overlay_1, overlay_2 = get_leather_armor_textures(ctx)
+
+    ctx.assets[CitArmorTexture]['minecraft:base_leather_armor_1'] = CitArmorTexture(leather_armor_1.copy())
+    ctx.assets[CitArmorTexture]['minecraft:base_leather_armor_2'] = CitArmorTexture(leather_armor_2.copy())
+    ctx.assets[CitArmorTexture]['minecraft:base_leather_armor_1_overlay'] = CitArmorTexture(overlay_1)
+    ctx.assets[CitArmorTexture]['minecraft:base_leather_armor_2_overlay'] = CitArmorTexture(overlay_2)
+
     leather_armor_1.putpixel((0, 1), (255, 255, 255))
     leather_armor_2.putpixel((0, 1), (255, 255, 255))
     fancy_armor_1: list[Image.Image] = [leather_armor_1]
@@ -59,11 +91,25 @@ def fancify(ctx: Context):
     for path, file in ctx.assets[ArmorTexture].items():
         trash.append(path)
 
+        filename = path.split(':')[1].split('.')[0]
         color = int(path.split('.')[-1])
         image: Image.Image = file.ensure_deserialized()
+
+        ctx.assets[CitArmorTexture][f'minecraft:armor_{color}_{filename[-1]}'] = CitArmorTexture(image.copy())
+        if filename.endswith('1'): # scuffed way to only do it once
+            ctx.assets[CitPropertiesFile][f'minecraft:armor_{color}'] = CitPropertiesFile('\n'.join([
+                "# Generated using https://github.com/PuckiSilver/FancyPantsLite-to-CIT by PuckiSilver",
+                "type=armor",
+                "weight=1",
+                "texture.leather_layer_1=empty",
+                f'texture.leather_layer_1_overlay=armor_{color}_1',
+                "texture.leather_layer_2=empty",
+                f'texture.leather_layer_2_overlay=armor_{color}_2',
+                "items=minecraft:leather_helmet minecraft:leather_chestplate minecraft:leather_leggings minecraft:leather_boots",
+                f'nbt.display.color={color}',""]))
+
         image.putpixel((0, 0), color_int_to_tuple(color))
-        
-        if path.split(':')[1].split('.')[0].endswith('1'):
+        if filename.endswith('1'):
             fancy_armor_1.append(image)
         else:
             fancy_armor_2.append(image)
